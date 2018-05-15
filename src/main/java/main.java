990d417a7d5802183a956265c3ed965f8326fb00
Andy W <wuandyw@sfu.ca>
2018-05-15 00:08:11 -0700

    Fixed Jsoup dependancies and added some sample code

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,25 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+import static com.sun.activation.registries.LogSupport.log;
+
 public class main {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
+        log(doc.title());
+
+        Elements newsHeadlines = doc.select("#mp-itn b a");
+        for (Element headline : newsHeadlines) {
+            log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
+        }
+    }
+
+    private static void log(String msg, String... vals) {
+        System.out.println(String.format(msg, vals));
+    }
 }
+
