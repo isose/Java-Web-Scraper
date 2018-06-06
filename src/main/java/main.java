8764d6e57f9956e00ee9e6c5a65b155c3c1e9110
Matthew Ng <theisose@gmail.com>
2018-06-06 01:28:41 -0700

    Scrapes Reddit subreddits for steam game urls.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,25 +1,17 @@
 import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+        import org.jsoup.nodes.Document;
+        import org.jsoup.nodes.Element;
+        import org.jsoup.select.Elements;
 
-import java.io.IOException;
-
-import static com.sun.activation.registries.LogSupport.log;
+        import java.io.IOException;
 
 public class main {
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("http://en.wikipedia.org/").get();
-        log(doc.title());
-
-        Elements newsHeadlines = doc.select("#mp-itn b a");
-        for (Element headline : newsHeadlines) {
-            log("%s\n\t%s", headline.attr("title"), headline.absUrl("href"));
+        Document doc = Jsoup.connect("https://www.reddit.com/r/FreeGameFindings/").get();
+        Elements steamUrls = doc.select("a[href*=store.steampowered][class*='s1yomx28-0']");
+        for (Element url : steamUrls) {
+            System.out.println("\nLink: " + url.attr("href"));
         }
-    }
-
-    private static void log(String msg, String... vals) {
-        System.out.println(String.format(msg, vals));
     }
 }
 
