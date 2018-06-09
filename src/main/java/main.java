d0b85cfc030fe871b0ca58df55361650629062a1
Matthew Ng <theisose@gmail.com>
2018-06-08 19:10:34 -0700

    Added method to print game's description given the game's url.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,16 +1,34 @@
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
         import org.jsoup.nodes.Document;
         import org.jsoup.nodes.Element;
         import org.jsoup.select.Elements;
-
         import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class main {
+
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://www.reddit.com/r/FreeGameFindings/").get();
-        Elements steamUrls = doc.select("a[href*=store.steampowered][class*='s1yomx28-0']");
+        Document doc = Jsoup.connect("https://www.reddit.com/r/steamdeals/").get();
+        Elements steamUrls = doc.select("a[href*=store.steampowered.com/app][class*='s1yomx28-0']");
+
+        Map<String, String> steamCookies = new HashMap<String, String>();
+        steamCookies.put("birthtime", "915177601");
+        steamCookies.put("mature_content", "1");
+
         for (Element url : steamUrls) {
-            System.out.println("\nLink: " + url.attr("href"));
+            String gameUrl = url.attr("href");
+            printDescription(gameUrl, steamCookies);
+            System.out.println("Link: " + gameUrl + "\n");
+        }
+    }
+
+    private static void printDescription(String gameUrl, Map<String, String> cookies) throws IOException{
+        Document doc = Jsoup.connect(gameUrl).cookies(cookies).get();
+        Elements steamUrls = doc.select(".game_description_snippet");
+        for (Element url : steamUrls) {
+            System.out.println(url.text());
         }
     }
 }
