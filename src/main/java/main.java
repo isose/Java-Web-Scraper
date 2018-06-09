c1b30c11bf42d2ac43717311a569ada52781c684
Andy Wu <www.flamer1959@hotmail.com>
2018-06-08 23:18:43 -0700

    Added a validator to check if the game is free

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,9 @@
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
-        import org.jsoup.nodes.Document;
-        import org.jsoup.nodes.Element;
-        import org.jsoup.select.Elements;
-        import java.io.IOException;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -19,17 +19,28 @@ public class main {
 
         for (Element url : steamUrls) {
             String gameUrl = url.attr("href");
-            printDescription(gameUrl, steamCookies);
-            System.out.println("Link: " + gameUrl + "\n");
+            if(isGameFree(gameUrl, steamCookies)) {
+                printDescription(gameUrl, steamCookies);
+                System.out.println("Link: " + gameUrl + "\n");
+            }
         }
+    }
+
+    private static boolean isGameFree(String gameUrl, Map<String, String> cookies) throws IOException {
+        String freeGameText = "Install Game";
+        String freeToPlay = "Play Game";
+        Document doc = Jsoup.connect(gameUrl).cookies(cookies).get();
+        Elements priceBox = doc.select("[class~=btnv6_green_white_innerfade]");
+        return (priceBox.text().equals(freeGameText) || priceBox.text().equals(freeToPlay));
     }
 
     private static void printDescription(String gameUrl, Map<String, String> cookies) throws IOException{
         Document doc = Jsoup.connect(gameUrl).cookies(cookies).get();
         Elements steamUrls = doc.select(".game_description_snippet");
-        for (Element url : steamUrls) {
-            System.out.println(url.text());
-        }
+        System.out.println(steamUrls.text());
+
+
     }
+
 }
 
