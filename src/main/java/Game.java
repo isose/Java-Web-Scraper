7549bd7058e95c560bf4ae4d1df3cb001e4c9b3e
Andy Wu <www.flamer1959@hotmail.com>
2018-06-17 20:43:45 -0700

    Forgot to add the two classes.

---
+++ src/main/java/Game.java
@@ -0,0 +1,65 @@
+public class Game {
+    private String url;
+    private String name;
+    private String description;
+    private String rating;
+    private String price;
+    private String imageUrl;
+
+    public Game(String url, String name, String description, String rating, String price, String imageUrl) {
+        this.url = url;
+        this.name = name;
+        this.description = description;
+        this.rating = rating;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
---
+++ src/main/java/redditScraper.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+public class redditScraper {
+    private HashSet<String> subredditUrl;
+
+
+
+    public HashSet<String> getSteamUrls() {
+        HashSet<String> listOfSteamUrls = new HashSet<String>();
+
+        return listOfSteamUrls;
+    }
+
+}
