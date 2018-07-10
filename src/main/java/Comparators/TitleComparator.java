9932812dc68c1d05de722fe588e51874d341ead7
Matthew Ng <theisose@gmail.com>
2018-07-09 17:51:47 -0700

    Added TitleComparator

---
+++ src/main/java/Comparators/TitleComparator.java
@@ -0,0 +1,13 @@
+package Comparators;
+
+import Models.SteamGame;
+
+import java.util.Comparator;
+
+public class TitleComparator implements Comparator<SteamGame> {
+
+    @Override
+    public int compare(SteamGame o1, SteamGame o2) {
+        return o1.getTitle().compareTo(o2.getTitle());
+    }
+}
