e7d104396eedb302c142078b0674abfe1326fda8
Matthew Ng <theisose@gmail.com>
2018-09-22 17:59:52 -0700

    Changes to RedditScraper.java to fix scraping duplicate games

--- src/main/java/Models/RedditScraper.java
+++ src/main/java/Models/RedditScraper.java
@@ -7,35 +7,51 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RedditScraper {
-    private HashSet<String> subRedditUrls;
+    private static final String STEAM_URL_PATTERN = "https://store.steampowered.com/app/\\d+";
+
+    private Set<String> subRedditUrls;
+    private HashSet<String> listOfSteamUrls;
 
     public RedditScraper(HashSet<String> subredditUrls) {
         this.subRedditUrls = subredditUrls;
+        listOfSteamUrls = new HashSet<>();
     }
-    private HashSet<String> listOfSteamUrls = new HashSet<>();
 
     public HashSet<String> getSteamUrls() throws IOException {
-
         for(String subReddit : subRedditUrls) {
             //Go to the subreddit page and select all steam pages on that subreddit
             Document doc = Jsoup.connect(subReddit).get();
-            Elements redditUrls = doc.select("a[href*=store.steampowered.com/app][class*=' ']");
-            urlScrape(redditUrls);
-            obtainUrlsFromTable(doc);
+            Elements redditElements = doc.select("a[href*=store.steampowered.com/app][class*=' ']");
+            listOfSteamUrls.addAll(getUrlSetFromElements(redditElements));
         }
         return listOfSteamUrls;
     }
 
-    private void urlScrape(Elements redditUrls) {
+    private Set<String> getUrlSetFromElements(Elements redditUrls) {
+        Set<String> urlSet = new HashSet<>();
+        Pattern pattern = Pattern.compile(STEAM_URL_PATTERN);
         //For each element selected add it's url to the list of urls
         for(Element url : redditUrls) {
-            String gameUrl = url.attr("href");
-            listOfSteamUrls.add(gameUrl);
+            //Extracts substring of the url containing up to the game id to avoid duplicates of the same url with different ending
+            Matcher matcher = pattern.matcher(url.attr("href"));
+            if (matcher.find()) {
+                urlSet.add(matcher.group());
+            }
         }
+        return urlSet;
     }
 
+    /**
+     * scrapes urls from subReddits containing a table
+     * @param doc
+     * @deprecated used only during Steam summer sale
+     */
+    @Deprecated
     private void obtainUrlsFromTable(Document doc) {
         Elements redditUrls = doc.select("td a");
         for(Element url : redditUrls) {
